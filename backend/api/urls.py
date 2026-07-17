1f065d676b951afda44a7197df55b4e3783cec4c
supunijayasinghe <supuninavodya695@gmail.com>
2026-07-17 10:48:24 +0530

    Implement JWT authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import MeView, RegisterView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('me/', MeView.as_view(), name='me'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'username': request.user.username})
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            {
+                'message': 'User created successfully',
+                'username': user.username,
+            },
+            status=status.HTTP_201_CREATED
+        )
+    
+    
